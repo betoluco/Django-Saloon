6ea4accc1f7a7206e5d7be186fb09da162c01279
Betoluco <betoluco@hotmail.com>
2017-06-07 17:43:44 -0500

    Display profile images in the templates
    Add an <img> to template and modify the urls to display media images

--- website/urls.py
+++ website/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 from places import views
 
@@ -22,4 +24,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.home_page, name='home_page'),
     url(r'^results/', views.results, name='results')
-]
+] + static(settings.MEDIA_URL, 
+document_root=settings.MEDIA_ROOT)
