18e80c19ff48883088fd24840625248d71097440
Betoluco <betoluco@hotmail.com>
2017-06-30 12:01:32 -0500

    Add static files for palce "template"

--- places/static/test/tests.js
+++
@@ -1,8 +0,0 @@
-QUnit.test( "Sanity check", function( assert ) {
-  assert.ok( 1 == "1", "Passed!" );
-});
-
-QUnit.test("readCookie retrieves the cookie content", function(assert){
-	document.cookie = "test=Test cookie";
-	assert.equal(readCookie("test"), "Test cookie");
-});
