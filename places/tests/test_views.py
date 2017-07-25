0ad4208f086de222efe9e8bbec17769218f35f6d
Carlos Alberto Hurtado <betoluco@hotmail.com>
2017-07-25 14:54:22 -0500

    Refactor the code for better performance and mobile display
    The code from the add_markers was integrated in results.html
    Change in the view that loads just the markers from the visible map

--- places/tests/test_views.py
+++ places/tests/test_views.py
@@ -9,32 +9,34 @@ class HomePageTest(TestCase):
 		response = self.client.get('/')
 		self.assertTemplateUsed(response, 'places/home_page.html')
 
-	def test_home_page_query_all_active_places(self):
-		response = self.client.get('/')
-		self.assertNotIn(Place.objects.get(pk=1), response.context['places'])
-
-	def test_results_view_queries_are_diplayed_in_groups_of_five_elements(self):
-		response = self.client.get('/results/?bounds={%22south%22:20, %22west%22:-100, %22north%22:19, %22east%22:-99}')
-		self.assertEqual(len(response.context['results']), 5)
-
-	def test_results_view_queries_for_active_places_in_the_boundaries_passed_in_URL(self):
+	def test_results_view_queries_all_active_markers_in_the_boundaries_passed_in_URL(self):
 		response = self.client.get('/results/?bounds={%22south%22:19.237373, %22west%22:-99.746390, %22north%22:19.410055, %22east%22:-99.520312}')
-		self.assertEqual(len(response.context['results']), 4)
+		self.assertEqual(len(response.context['markers']), 12)
 		objects = (Place.objects.get(pk=1), Place.objects.get(pk=2), Place.objects.get(pk=3))
 		for obj in objects:
-			self.assertNotIn(obj, response.context['results'])
-	
-	def test_the_group_displayed_is_selected_through_a_keyword_argument_passed_in_the_url(self):
-		response = self.client.get('/results/?bounds={%22south%22:20, %22west%22:-100, %22north%22:19, %22east%22:-99}&page=2')
-		self.assertIn(Place.objects.get(pk=7), response.context['results'])
+			self.assertNotIn(obj, response.context['markers'])
 
-	def test_if_the_keyword_argument_passed_in_the_url_is_not_an_intege_the_first_page_is_returned(self):
-		response = self.client.get('/results/?bounds={%22south%22:20, %22west%22:-100, %22north%22:19, %22east%22:-99}&page=None')
-		objects = [
-			Place.objects.get(pk=2),
-			Place.objects.get(pk=3),
-			Place.objects.get(pk=4),
-			Place.objects.get(pk=5),
-			Place.objects.get(pk=6)
-			]
-		self.assertListEqual(objects, response.context['results'].object_list)
+	def test_results_view_returns_places_in_groups_of_ten_elements(self):
+		response = self.client.get('/results/?bounds={%22south%22:19.237373, %22west%22:-99.746390, %22north%22:19.410055, %22east%22:-99.520312}')
+		self.assertEqual(len(response.context['results']), 10)
+	
+	def test_results_view_returns_page_one_if_no_page_paremter_is_provided_in_the_url(self):
+		response = self.client.get('/results/?bounds={%22south%22:19.237373, %22west%22:-99.746390, %22north%22:19.410055, %22east%22:-99.520312}')
+		objects = (Place.objects.get(pk=4), Place.objects.get(pk=5), Place.objects.get(pk=6),\
+			Place.objects.get(pk=7), Place.objects.get(pk=8), Place.objects.get(pk=9),\
+			Place.objects.get(pk=10), Place.objects.get(pk=11), Place.objects.get(pk=12),\
+			Place.objects.get(pk=13))
+		print(response.context['results'].object_list)
+		for obj in objects:
+			self.assertIn(obj, response.context['results'].object_list)
+
+	def test_results_view_returns_the_page_selected_through_a_keyword_argument_passed_in_the_url(self):
+		response = self.client.get('/results/?bounds={%22south%22:19.237373, %22west%22:-99.746390, %22north%22:19.410055, %22east%22:-99.520312}&page=2')
+		self.assertEqual(len(response.context['results']), 2)
+		objects = (Place.objects.get(pk=14), Place.objects.get(pk=15))
+		for obj in objects:
+			self.assertIn(obj, response.context['results'].object_list)
+
+	def test_place_view_returns_place_requested_in_the_url(self):
+		response = self.client.get('/place/?place=4')
+		self.assertEqual(Place.objects.get(pk=4), response.context['place'])
--- places/views.py
+++ places/views.py
@@ -5,20 +5,18 @@ from django.core.paginator import Paginator, PageNotAnInteger
 
 from places.models import Place
 
+PER_PAGE = 10
 
 def home_page(request):
-	return render(request, 'places/home_page.html', {'places':Place.objects.filter(active=True)})
+	return render(request, 'places/home_page.html')
 
 def results(request):
 	box = json.loads(request.GET['bounds'])
 	poly = Polygon.from_bbox((box['west'], box['south'], box['east'], box['north']))
-	paginator =	Paginator(Place.objects.filter(active=True).filter(latitude_longitude__within=poly), 5)
-	page = request.GET.get('page')
-	try:
-		results = paginator.page(page)
-	except PageNotAnInteger:
-		results = paginator.page(1)
-	return render(request, 'places/results.html', {'results': results})
+	markers = Place.objects.filter(active=True).filter(latitude_longitude__within=poly)
+	results = Paginator(markers, PER_PAGE)
+	page = results.page(request.GET.get('page', 1))
+	return render(request, 'places/results.html', {'results': page, 'markers': markers})
 
 def place(request):
 	place = Place.objects.get(pk=request.GET['place'])
